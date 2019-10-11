6f655c700fdb1c5a677f9949642917c7820a70d9
ssing072 <36577991+ssing072@users.noreply.github.com>
2019-10-11 14:50:51 -0700

    Update main.c

--- Lab4_SMs/source/main.c
+++ Lab4_SMs/source/main.c
@@ -15,62 +15,60 @@
 int main(void) {
     /* Insert DDR and PORT initializations */
     DDRA = 0x00; PORTA = 0xFF;
-    DDRB = 0xFF; PORTB = 0x00;
+    DDRC = 0xFF; PORTC = 0x00;
     
-    enum states {init, first_wait, light_1, light_2, next_wait} state;
+    enum states {INIT, WAIT, INCREMENT, DECREMENT} state;
     state = init;
     /* Insert your solution below */
     while (1) {
         switch(state){
-            case init:
-                state = first_wait;
+            case INIT:
+                state = WAIT;
                 break;
-            case first_wait:
+            case WAIT:
                 if((PINA & 0x01) == 1){
-                    state = light_2;
+                    state = INCREMENT;
                 }
+                else if((PINA & 0x02) == 1){
+                    state = DECREMENT;
+                }
+                else if((PINA == 0x03
                 else{
-                    state = first_wait;
+                    state = WAIT;
                 }
                 break;
-            case light_1:
+            case INCREMENT:
                 if(PINA == 0){
-                    state = first_wait;
+                    state = WAIT;
                 }
                 else{
-                    state = light_1;
+                    state = INCREMENT;
                 }
                 break;
-           case light_2:
+            case DECREMENT:
                 if(PINA == 0){
-                    state = next_wait;
+                    state = WAIT;
                 }
                 else{
-                    state = light_2;
+                    state = INCREMENT;
                 }
                 break;
-           case next_wait:
-                if((PINA & 0x01) == 1){
-                    state = light_1;
+        }   
+        switch(state){
+            case INIT:
+                PORTC = 7;
+                break;
+            case WAIT:
+                break;
+            case INCREMENT:
+                if(PORTC < 9){
+                    PORTC = PORTC + 1;
                 }
-                else{
-                    state = next_wait;
+                break;
+            case DECREMENT:
+                if(PORTC > 0){
+                    PORTC = PORTC + 1;
                 }
-                break;
-        }
-        switch (state) {
-            case init:
-                PORTB = 0x01;
-                break;
-            case first_wait:
-                break;
-            case next_wait:
-                break;
-            case light_1:
-                PORTB = 0x01;
-                break;
-            case light_2:
-                PORTB = 0x02;
                 break;
         }
     }
