18fb78ba63f29e179c55b81b8a8349e735bf4fa3
Surya Singh <ssing072@wch136-02.cs.ucr.edu>
2019-10-11 19:33:08 -0700

    Completed Part 4

---
+++ Lab4_SMs/turnin/ssing072_lab4_part2.c
@@ -0,0 +1,76 @@
+/*	Author: ssing072
+ *  Partner(s) Name: Neha Gupta
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRC = 0xFF; PORTC = 0x00;
+    
+    enum states {INIT, WAIT, INCREMENT, DECREMENT, RESET} state;
+    state = INIT;
+    unsigned char tempOut = 0x00;
+    tempOut = 7;
+    /* Insert your solution below */
+    while (1) {
+        switch(state){
+            case INIT:
+                state = WAIT;
+                break;
+            case WAIT:
+                if(PINA == 1){
+                    state = INCREMENT;
+                }
+                else if(PINA == 2){
+                    state = DECREMENT;
+                }
+                else if(PINA == 0){
+                    state = RESET;    
+                }
+                else{
+                    state = WAIT;
+                }
+                break;
+            case INCREMENT:
+                state = WAIT;
+                break;
+            case DECREMENT:
+                state = WAIT;
+                break;
+            case RESET:
+                state = WAIT;
+        }   
+        switch(state){
+            case INIT:
+                tempOut = 7;
+                break;
+            case WAIT:
+                break;
+            case RESET:
+               tempOut = 0;
+                break;
+            case INCREMENT:
+                if(tempOut < 9){
+                    tempOut = tempOut + 1;
+                }
+                break;
+            case DECREMENT:
+                if(tempOut > 0){
+                    tempOut = tempOut - 1;
+                }
+                break;
+        }
+        PORTC = tempOut;
+    }
+    return 1;
+}
